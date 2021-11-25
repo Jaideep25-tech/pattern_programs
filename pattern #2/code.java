61ed2c5faea2e80790974893457929295b2a6c1e
Jaideep Singh <79747022+Jaideep25-tech@users.noreply.github.com>
2021-11-25 10:15:03 -0800

    Update code.java

--- pattern #2/code.java
+++ pattern #2/code.java
@@ -1,1 +1,17 @@
-
+class pattern_113
+{
+  public static void main()
+  {
+    for(int i=1;i<=5;i++)
+    {
+      for(init j=1;j<=5;j++)
+      {
+        if(j==3||i==3)
+          System.out.println("* ");
+        else
+          System.out.println(" ");
+      }
+      System.out.println();
+    }
+  }
+}
