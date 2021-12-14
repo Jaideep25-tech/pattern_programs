4c0ed6a06e47b4e3da132a6245d7e0dd29920d8f
Jaideep Singh <79747022+Jaideep25-tech@users.noreply.github.com>
2021-12-14 09:29:31 -0800

    Create code.cpp

---
+++ patter #3/code.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int i,j;
+  for(i=1;i<=5;i++)
+  {
+    for(j=1;j<=5;j++)
+    {
+      if(i==3&&j==3)
+        cout<<"o ";
+      else
+        cout<<"x ";
+    }
+    getch();
+  }
