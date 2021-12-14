71ffe8c8d5550630eeb4a786f23403716ba3287f
Jaideep Singh <79747022+Jaideep25-tech@users.noreply.github.com>
2021-12-14 09:22:23 -0800

    Update code.java

--- patter #3/code.java
+++ patter #3/code.java
@@ -1,1 +1,19 @@
-
+class patter3
+{
+  public static void main()
+  {
+    int n=5;
+    int m=(n/2)+1;
+    for(int i=1;i<=n;i++)
+    {
+      for(int j=1;j<=n;j++)
+      {
+        if(i==m&&j==m)
+          System.out.print("o ");
+        else
+          System.out.print("x ");
+      }
+      System.out.ptintln();
+    }
+  }
+}
