43f36c48a20718a775fe9dc66ac9858f165c1d05
Jaideep Singh <79747022+Jaideep25-tech@users.noreply.github.com>
2021-12-26 23:06:23 -0800

    Update code.java

--- pattern #4/code.java
+++ pattern #4/code.java
@@ -1,1 +1,21 @@
-
+class pattern{
+  public static void main()
+  {
+    int n=5;
+    for(int i=1;i<=n;i++)
+    {
+      for(int j=1;j<=n;j++)
+      {
+        if(i==j||i+j==n+1)
+        {
+          System.out.print("x ");
+        }
+        else
+        {
+          System.out.print("o ");
+        }
+      }
+      System.out.println();
+    }
+  }
+}
