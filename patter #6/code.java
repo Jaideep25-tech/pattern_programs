0c53602b0503edfc9bbb66802d4bc5df5bcebcd2
Jaideep Singh <79747022+Jaideep25-tech@users.noreply.github.com>
2022-01-01 20:20:30 -0800

    Create code.java

---
+++ patter #6/code.java
@@ -0,0 +1,21 @@
+class pattern
+{
+  public static void main()
+  {
+    int z=1;
+    int n=5;
+    for(int i=0;i<n;i++)
+    {
+      for(int j=n-1;j>i;j--)
+      {
+        System.out.print(" ");
+      }
+      for(int k-0;k<n;k++)
+      {
+        System.out.print("*");
+      }
+      z++;
+      System.out.println();
+    }
+  }
+}
