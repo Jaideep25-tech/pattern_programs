6f8545224966ceffb1cb762dfe7cc5e7413fc527
Jaideep Singh <79747022+Jaideep25-tech@users.noreply.github.com>
2022-01-01 20:25:53 -0800

    Create code.cpp

---
+++ patter #6/code.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int z=1;
+  int n=5;
+  for(int i=0;i<n;i++)
+  {
+    for(int j=n-1;j>i;j--)
+    {
+      cot<<" ";
+    }
+    for(int k=0;k<n;k++)
+    {
+      cout<<"*";
+    }
+    z++;
+    cout<<endl;
+  }
+  getch();
+}
