79a2ed32f868a8cf0dcbb75b6b2c8ba5eae70bba
Jaideep Singh <79747022+Jaideep25-tech@users.noreply.github.com>
2022-10-05 08:11:34 +0530

    Update code.cpp

--- pattern #9/code.cpp
+++ pattern #9/code.cpp
@@ -1,1 +1,15 @@
-
+int main()
+{
+  int n=6;
+  int j,c,i,k;
+  for(i=n;i>0;i--)
+  {
+    k=(i%2!=0)?i+1:i;
+    for(j=0;j<k;j++)
+    {
+      printf("*");
+    }
+    printf("\n");
+  }
+  getch()
+  }
